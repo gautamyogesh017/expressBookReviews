9c52ec05443896bfd20b8756dc8590adf8a7dafb
Yogesh Gautam <gautamyogesh017@gmail.com>
2024-01-22 12:00:37 +0545

    auth

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+if(req.session.auth){
+    token = req.session.auth['accessToken']
+
+//token verification
+jwt.verify(token,'secret123',(err,user)=>{
+    if(!err){
+        req.user = user
+        next()
+    }
+    else {
+        return res.status(403).json({message:"User is not authenticated"})
+    }
+})
+}    else {
+    return res.status(403).json({message: "User not logged in"})
+}
 });
+
  
 const PORT =5000;
 
